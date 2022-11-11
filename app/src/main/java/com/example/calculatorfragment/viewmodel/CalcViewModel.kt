6e44cd7bcd2c6d50f0b82a57ada47f03fbed89ac
alyonagrigor <76869347+alyonagrigor@users.noreply.github.com>
2022-11-11 17:38:38 +0700

    add negate function

--- app/src/main/java/com/example/calculatorfragment/viewmodel/CalcViewModel.kt
+++ app/src/main/java/com/example/calculatorfragment/viewmodel/CalcViewModel.kt
@@ -213,9 +213,10 @@ class CalcViewModel : ViewModel() {
         _stringInput.value = "" //очищаем строку дял следующей операции
     }
 
+    /*обрезаем .0 при выводе в input*/
     private fun cutZeroOutput(d: Double) {
 
-        var sD = d.toString() // обрезаем .0 при выводе в input
+        var sD = d.toString()
         if (sD.endsWith(".0")) {
             val x = sD.indexOf(".")
             sD = sD.substring(0, x)
@@ -224,8 +225,8 @@ class CalcViewModel : ViewModel() {
         _showInput.value = _stringInput.value //_num2 отображаем в строке input
     }
 
-   /*прочие мат. операции*/
 
+    /*прочие мат. операции*/
     fun percent() {
 
         if (_hasNum1.value == false && _stringInput.value == "") {
@@ -235,7 +236,8 @@ class CalcViewModel : ViewModel() {
         } else if (_hasNum1.value == false) { //если введено только 1 число, а кнопка арифметической операции
             // не нажата, то рассчитать 1 процент
             _num1.value = _stringInput.value!!.toDouble()
-            _sHistory.value = _sHistory.value + "\u200b * 1% \u200b" //записываем в историю, что мы рассчитали 1%
+            _sHistory.value =
+                _sHistory.value + "\u200b * 1% \u200b" //записываем в историю, что мы рассчитали 1%
             _num2.value = _num1.value!!.div(100)
             cutZeroOutput(_num2.value!!) //обрезаем ноль, если нужно, и выводим в строке input
             _isLastPressedOperation.value = false
@@ -251,7 +253,8 @@ class CalcViewModel : ViewModel() {
 
         } else if (_stringInput.value != ""
             && _operator.value != '0'
-            && _isLastPressedOperation.value == false) {
+            && _isLastPressedOperation.value == false
+        ) {
             //если заданы 2 операнда и арифметическая операция в operator - рассчитываем
             // _num2.value процентов от _num1.value
             _num2.value = _stringInput.value!!.toDouble()
@@ -282,7 +285,8 @@ class CalcViewModel : ViewModel() {
 
         } else if (_isBSAvailable.value == true
             && _stringInput.value!!.length == 3
-            && _stringInput.value!!.get(1) != '-') {
+            && _stringInput.value!!.get(1) != '-'
+        ) {
             //если в строке 3 символа и второй - не минус, то можно стирать последний
             //символ безопасно
             _stringInput.value = _stringInput.value!!.dropLast(1)
@@ -290,7 +294,8 @@ class CalcViewModel : ViewModel() {
 
         } else if (_isBSAvailable.value == true
             && _stringInput.value!!.length == 3
-            && _stringInput.value!!.get(1) != '-') {
+            && _stringInput.value!!.get(1) != '-'
+        ) {
             //если в строке 3 символа и второй - минус, то можно стирать все 3 символа:
             //цифру, минус, пробел
             _stringInput.value = "0"
@@ -298,7 +303,8 @@ class CalcViewModel : ViewModel() {
             _isBSAvailable.value = false
 
         } else if (_isBSAvailable.value == true
-            && _stringInput.value!!.length == 1) {
+            && _stringInput.value!!.length == 1
+        ) {
             //если в строке осталась только 1 цифра, заменяем ее на ноль
             _stringInput.value = "0"
             _sHistory.value = _sHistory.value!!.dropLast(1)
@@ -308,65 +314,90 @@ class CalcViewModel : ViewModel() {
     }
 
 
-//    fun negate() {
-//        if (_hasNum1.value == false
-//            && _stringInput.value == "") { //если еще не была введена ни одна цифра
-//            //TODO   showToastFirstDigit()
-//
-//        } else if (_isLastPressedOperation.value == true) {
-//            //если нажат арифметический оператор, а новое число не введено
-//            //TODO  showToastNextDigit()
-//
-//        } else if (_hasNum1.value == false
-//            && _stringInput.value[0] == '-') {
-//            //если Negate нажимают когда ввели первое число с минусом
-//            sbHistory.delete(0, sbHistory.length) //стираем число из строки sbHistory
-//            _stringInput.value = _stringInput.value.substring(1) //обрезаем минус и пробел в строке _stringInput.value
-//            _showInput.value = _stringInput.value
-//            sendSInputToJournal()
-//
-//        } else if (_hasNum1.value == false
-//            && _stringInput.value[0] != '-') {
-//            //если Negate нажимают когда ввели первое число положительное
-//            sbHistory.delete(0, sbHistory.length) //стираем число из строки sbHistory
-//            _stringInput.value = "-$_stringInput.value" //добавляем минус в строке _stringInput.value
-//            sendSInputToJournal()
-//
-//        } else if (_hasNum1.value && _stringInput.value == "" && binding.etInput.text.toString() != "") {
-//            //если введенное в строке число - результат предыдущих вычислений
-//            _stringInput.value = binding.etInput.text.toString() //получаем значение из текстового поля
-//            //т.к. цепочка вычислений перезатирается, перезаписываем sbHistory
-//            sbHistory.delete(0, sbHistory.length)
-//            if (_stringInput.value[0] == '-') { //если получили отриц. число в
-//                //результате предыдущих операций
-//                _stringInput.value = _stringInput.value.substring(1) //обрезаем минус в строке _stringInput.value
-//            } else if (_stringInput.value[0] != '-') {
-//                _stringInput.value = "-$_stringInput.value" //добавляем минус в строке _stringInput.value
-//            }
-//            sendSInputToJournal()
-//            _operator.value = '0'
-//            _hasNum1.value = false
-//
-//            /*стандартные случаи, когда Negate нажат после числа по ходу вычислений*/
-//        } else if (_stringInput.value[0] != '-'
-//            && _operator.value != '0') {
-//            val x = sbHistory.lastIndexOf(_stringInput.value) //получаем индекс, чтобы обрезать текущее число
-//            sbHistory.delete(x, sbHistory.length) //обрезаем
-//            _stringInput.value = "-$_stringInput.value" //добавляем минус в строке _stringInput.value
-//            sbHistory.append("\u200b").append("(").append(_stringInput.value).append(")") //и вставляем снова
-//            // в sbHistory вместе с пробелом и добавляем скобки
-//
-//        } else if (_stringInput.value[0] == '-' && operator != '0') {
-//            val x = sbHistory.lastIndexOf(_stringInput.value)
-//            sbHistory.delete(x - 2, sbHistory.length) //обрезаем число с минусом и скобками
-//            _stringInput.value = _stringInput.value.substring(1) //обрезаем минус в строке _stringInput.value
-//            sendSInputToJournal()
-//        }
-//        _showInput.value = _stringInput.value
-//        _isLastPressedOperation.value = false
-//        _isBSAvailable.value = false
-//    }
+    fun negate() {
+        if (_hasNum1.value == false
+            && _stringInput.value == ""
+        ) { //если еще не была введена ни одна цифра
 
+            //TODO   showToastFirstDigit()
+
+        } else if (_isLastPressedOperation.value == true) {
+            //если нажат арифметический оператор, а новое число не введено
+
+            //TODO тут пропадает число из строки
+
+            //TODO  showToastNextDigit()
+
+        } else if (_hasNum1.value == false
+            && _stringInput.value!!.get(0) == '-'
+        ) {
+            //если Negate нажимают когда ввели первое число отрицательное кнопкой минус
+
+            _stringInput.value =
+                _stringInput.value!!.drop(1) //обрезаем минус в строке _stringInput.value
+            _showInput.value = _stringInput.value
+            _sHistory.value = _stringInput.value //записываем результат в sHistory
+
+        } else if (_hasNum1.value == false
+            && _stringInput.value!!.get(0) != '-'
+        ) {
+            //если Negate нажимают когда ввели первое число положительное
+
+            _stringInput.value =
+                "-" + _stringInput.value //добавляем минус в строке _stringInput.value
+            _showInput.value = _stringInput.value
+            _sHistory.value = "\u200b" + _stringInput.value //записываем результат в sHistory
+
+        } else if (_hasNum1.value == true
+            && _stringInput.value == ""
+            && _showInput.value != ""
+        ) {
+            //если введенное в строке число - результат предыдущих вычислений
+
+            _stringInput.value = _showInput.value //получаем значение из текстового поля
+            //т.к. цепочка вычислений перезатирается, стираем историю предыдущих операций в sHistory
+            _sHistory.value = ""
+            if (_stringInput.value!!.get(0) == '-') { //если получили отриц. число в
+                //результате предыдущих операций
+                _stringInput.value =
+                    _stringInput.value!!.drop(1) //обрезаем минус в строке _stringInput.value
+            } else if (_stringInput.value!!.get(0) != '-') {
+                _stringInput.value =
+                    "-" + _stringInput.value //добавляем минус в строке _stringInput.value
+            }
+            _showInput.value = _stringInput.value
+            _sHistory.value = _stringInput.value //записываем результат в sHistory
+            _operator.value = '0'
+            _hasNum1.value = false
+
+            /*стандартные случаи, когда Negate нажат после числа по ходу вычислений*/
+        } else if (_stringInput.value!!.get(0) != '-'
+            && _operator.value != '0'
+        ) {
+            //проверка, что сейчас введено положительное число, тогда добавляем минус
+            val x =
+                _stringInput.value!!.length //получаем длину числа в строке, чтобы обрезать его из истории
+            _sHistory.value = _sHistory.value!!.dropLast(x) //обрезаем
+            _stringInput.value =
+                "-$_stringInput.value" //добавляем минус в строке _stringInput.value
+            _sHistory.value =
+                _sHistory.value.plus("\u200b").plus("($_stringInput.value)")
+            //и вставляем снова в sHistory вместе с пробелом и добавляем скобки
+
+        } else if (_stringInput.value!!.get(0) == '-' && _operator.value != '0') {
+            //проверка, что сейчас введено отрицательное число, тогда убираем минус
+            val x =
+                _stringInput.value!!.length //получаем длину числа в строке, чтобы обрезать его из истории
+            _sHistory.value = _sHistory.value!!.dropLast(x) //обрезаем
+
+            _stringInput.value =
+                _stringInput.value!!.drop(1) //обрезаем минус в строке _stringInput.value
+        }
+
+        _showInput.value = _stringInput.value
+        _isLastPressedOperation.value = false
+        _isBSAvailable.value = false
+    }
 
 
 }
